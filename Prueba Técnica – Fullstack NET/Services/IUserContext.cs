5eaddcf64cc8d50cefe186859e01679ce7ec2600
Herrera-Nicolas <zambottinico@gmail.com>
2025-11-18 17:54:17 -0300

    Add UserContext service

---
+++ Prueba Técnica – Fullstack NET/Services/IUserContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Prueba_Técnica___Fullstack_NET.Services
+{
+    public interface IUserContext
+    {
+    }
+}
---
+++ Prueba Técnica – Fullstack NET/Services/UserContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Prueba_Técnica___Fullstack_NET.Services
+{
+    public class UserContext : IUserContext
+    {
+        private readonly IHttpContextAccessor _accessor;
+        private const string RoleKey = "Role";
+        private const string EmailKey = "Email";
+
+        public UserContext(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        private ISession? Session => _accessor.HttpContext?.Session;
+
+        public string CurrentRole => Session?.GetString(RoleKey) ?? "Usuario";
+        public string? Email => Session?.GetString(EmailKey);
+        public bool IsAuthenticated => !string.IsNullOrEmpty(Session?.GetString(RoleKey));
+    }
+}
